1518daa565f49bfa547be779872557e036dd9d51
Kris_Chan <kris.dacpc@gmail.com>
2017-11-20 20:17:05 +0800

    1.update the structure;

--- regression.py
+++ regression.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 '''
-Desc:  the prediction of NINO3.4
+Desc:  the prediction of NINO3.4, a regression problem.
 Author: Kris Peng
 Copyright (c) 2017 - Kris Peng <kris.dacpc@gmail.com>
 '''
@@ -21,30 +21,21 @@ STwind = read.read_csv(path=path, sep='\t', date_key='date_time')
 
 raw_data = pd.concat([NINO, STwind], axis=1).dropna(axis=0)
 
-# X, y = regression_set(NINOTrain, target_key='NINO3', initial_time=1961, horizon=1)
-# model = keras_ANN.KerasRegressionModel(arity=1, network_structure=(5, 1), batch_size=1, nb_epoch=1)
-# model.fit(X, y)
-# yhat = model.predict(X)
-# print(len(yhat))
-raw_data = pd.concat([NINO, STwind], axis=1).dropna(axis=0)
-
-X, y = regression_set(raw_data, target_key='NINO3', initial_time=2004, horizon=1)
-model = keras_ANN.KerasRegressionModel(arity=3, network_structure=(5, 1), batch_size=1, nb_epoch=1000)
+X, y = regression_set(raw_data, target_key='NINO3', initial_time=2003.5, horizon=1)
+model = keras_ANN.KerasRegressionModel(arity=3, network_structure=(5, 1), batch_size=1, nb_epoch=20000)
 model.fit(X, y)
 yhat = model.predict(X)
-
-plt.plot(range(len(yhat)), yhat, '-', color='blue', label="predicted")
-plt.plot(range(len(y)), y, '--', color='red', label="actual")
-# plt.legend(loc='upper left')
 
 # calculate the cost(NRMSE)
 cost = 0
 for n in range(len(y)):
     cost += (y[n] - yhat[n])**2
-
 cost = (1/(max(y) - min(yhat)) * ((cost/len(yhat))**0.5))
 print("NRMSE: %f" %cost)
 
+# plot
+plt.plot(range(len(y)), y, '--', color='red', label="actual")
+plt.plot(range(len(yhat)), yhat, '-', color='blue', label="predicted")
 plt.legend(loc='upper right')
 plt.show()
 
