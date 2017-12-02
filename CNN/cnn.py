aaf351b54012f26de73f81d04ee23666e8b0713b
Kris_Chan <kris.dacpc@gmail.com>
2017-12-02 19:03:54 +0800

    1.add the file loader;

---
+++ CNN/cnn.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+'''
+Desc:  apply CNN on the dataset, based on TensorFlow
+Author: Kris Peng
+Copyright (c) 2017 - Kris Peng <kris.dacpc@gmail.com>
+'''
+
+from netCDF4 import Dataset
+
+data = Dataset("../data/sst/sst.mon.mean.nc")
+
+# the basic information
+# print(data.data_model)
+# print(data.dimensions.keys())
+# print(data.dimensions['time'])
+# print(data.variables['sst'])
+
+sst = data.variables['sst']
+print(sst)
+# print(sst[1][:][:])
+
+
+data.close()
