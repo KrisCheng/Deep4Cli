b67189e7a4ad93c893d5e5e5d5b0a5bfb459d071
Kris_Chan <kris.dacpc@gmail.com>
2018-05-08 19:15:54 +0800

    1.add the outline of paper;

--- cnn_model/conv2d.py
+++ cnn_model/conv2d.py
@@ -5,43 +5,40 @@ Data: https://www.esrl.noaa.gov/psd/data/gridded/data.cobe2.html
 Copyright (c) 2018 - Kris Peng <kris.dacpc@gmail.com>
 '''
 
-from keras.models import Sequential
-from keras.layers.convolutional import Conv3D
-from keras.layers.convolutional_recurrent import ConvLSTM2D
-from keras.layers.normalization import BatchNormalization
+# from keras.models import Sequential
+# from keras.layers.convolutional import Conv3D
+# from keras.layers.convolutional_recurrent import ConvLSTM2D
+# from keras.layers.normalization import BatchNormalization
 import pylab as plt
-from pandas import DataFrame
-from pandas import concat
-from pandas import datetime
 from matplotlib import pyplot
 from netCDF4 import Dataset
 import numpy as np
+import pandas as pd
 import matplotlib as mpl
 import scipy.io as sio 
 
-seq = Sequential()
-seq.add(ConvLSTM2D(filters=40, kernel_size=(3, 3),
-                   input_shape=(None, 10, 50, 1),
-                   padding='same', return_sequences=True))
-seq.add(BatchNormalization())
+# seq = Sequential()
+# seq.add(ConvLSTM2D(filters=40, kernel_size=(3, 3),
+#                    input_shape=(None, 10, 50, 1),
+#                    padding='same', return_sequences=True))
+# seq.add(BatchNormalization())
 
-seq.add(ConvLSTM2D(filters=40, kernel_size=(3, 3),
-                   padding='same', return_sequences=True))
-seq.add(BatchNormalization())
+# seq.add(ConvLSTM2D(filters=40, kernel_size=(3, 3),
+#                    padding='same', return_sequences=True))
+# seq.add(BatchNormalization())
 
-seq.add(ConvLSTM2D(filters=40, kernel_size=(3, 3),
-                   padding='same', return_sequences=True))
-seq.add(BatchNormalization())
+# seq.add(ConvLSTM2D(filters=40, kernel_size=(3, 3),
+#                    padding='same', return_sequences=True))
+# seq.add(BatchNormalization())
 
-seq.add(ConvLSTM2D(filters=40, kernel_size=(3, 3),
-                   padding='same', return_sequences=True))
-seq.add(BatchNormalization())
+# seq.add(ConvLSTM2D(filters=40, kernel_size=(3, 3),
+#                    padding='same', return_sequences=True))
+# seq.add(BatchNormalization())
 
-seq.add(Conv3D(filters=1, kernel_size=(3, 3, 3),
-               activation='sigmoid',
-               padding='same', data_format='channels_last'))
-seq.compile(loss='binary_crossentropy', optimizer='adadelta')
-
+# seq.add(Conv3D(filters=1, kernel_size=(3, 3, 3),
+#                activation='sigmoid',
+#                padding='same', data_format='channels_last'))
+# seq.compile(loss='binary_crossentropy', optimizer='adadelta')
 
 
 # data preprocessing
@@ -49,14 +46,22 @@ sst = '../data/sst_grid/convert_sst.mon.mean_1850:01_2015:12.mat'
 sst_data = sio.loadmat(sst) 
 
 sst_data = sst_data['sst'][:,:,:]
-sst_data = np.array(sst_data)
+sst_data = np.array(sst_data, dtype=float)
+
+
 # (180 * 360 * 2004)
 # print(sst_data.shape)
 # (180 * 360 * 2004) --> (10 * 50 * 2004)
 sst_data = sst_data[85:95,190:240,:]
+
+# temp_sst = sst_data.reshape((50*10*2004))
+# series_sst_data = pd.Series(temp_sst)
+# print(series_sst_data.describe())
 # print(sst_data.shape)
 # print(sst_data[::,::,0])
-
+# print("Mean: %s" % sst_data.mean())
+# print("Max: %s , Loc: %s" % (sst_data.max(), sst_data.argmax()))
+# print("Min: %s , Loc: %s" % (sst_data.min(), sst_data.argmin()))
 # 1850.01~2015.01 (train)
 train_data = sst_data[::,::,0:-12]
 # 2015.01~2015.12 (test)
@@ -72,44 +77,48 @@ for i in range(166):
 noisy_movies = convert_sst
 shifted_movies = convert_sst
 
-seq.fit(noisy_movies[:160], shifted_movies[:160], batch_size=10,
-        epochs=1, validation_split=0.05)
+# seq.fit(noisy_movies[:160], shifted_movies[:160], batch_size=10,
+#         epochs=1, validation_split=0.05)
 
-# Testing the network on one movie
-# feed it with the first 7 positions and then
-# predict the new positions
-which = 100
-track = noisy_movies[which][:7, ::, ::, ::]
+# # Testing the network on one movie
+# # feed it with the first 7 positions and then
+# # predict the new positions
+# which = 100
+# track = noisy_movies[which][:7, ::, ::, ::]
 
-for j in range(12):
-    new_pos = seq.predict(track[np.newaxis, ::, ::, ::, ::])
-    new = new_pos[::, -1, ::, ::, ::]
-    track = np.concatenate((track, new), axis=0)
+# for j in range(12):
+#     new_pos = seq.predict(track[np.newaxis, ::, ::, ::, ::])
+#     new = new_pos[::, -1, ::, ::, ::]
+#     track = np.concatenate((track, new), axis=0)
 
 
-# And then compare the predictions
-# to the ground truth
-track2 = noisy_movies[which][::, ::, ::, ::]
+# # And then compare the predictions
+# # to the ground truth
+track2 = noisy_movies[165][::, ::, ::, ::]
 for i in range(12):
     fig = plt.figure(figsize=(10, 5))
 
-    ax = fig.add_subplot(121)
+#     ax = fig.add_subplot(121)
 
-    if i >= 7:
-        ax.text(1, 3, 'Predictions !', fontsize=20, color='w')
-    else:
-        ax.text(1, 3, 'Initial trajectory', fontsize=20)
+#     if i >= 7:
+#         ax.text(1, 3, 'Predictions !', fontsize=20, color='w')
+#     else:
+#         ax.text(1, 3, 'Initial trajectory', fontsize=20)
 
-    toplot = track[i, ::, ::, 0]
+#     toplot = track[i, ::, ::, 0]
 
-    plt.imshow(toplot)
-    ax = fig.add_subplot(122)
-    plt.text(1, 3, 'Ground truth', fontsize=20)
+#     plt.imshow(toplot)
+#     ax = fig.add_subplot(122)
+    plt.text(1, 3, 'Ground truth', fontsize=12)
 
     toplot = track2[i, ::, ::, 0]
-    if i >= 2:
-        toplot = shifted_movies[which][i - 1, ::, ::, 0]
-
+#     if i >= 2:
+    toplot = shifted_movies[165][i - 1, ::, ::, 0]
+    
     plt.imshow(toplot)
+    cbar = plt.colorbar(plt.imshow(toplot), orientation='horizontal')
+    cbar.set_label('°C',fontsize=12)
+    cbar.set_ticks(np.linspace(20,32,13)) 
+    cbar.set_ticklabels(('20','21','22','23','24','25','26','27','28','29','30','31','32'))
     plt.savefig('%i_animate.png' % (i + 1))
 
---
+++ outline.md
@@ -0,0 +1,81 @@
+## Using Deep Neural Networks to Study ENSO Case
+
+***Manuscript Version***Bin Mu, Cheng Peng and Shijin Yuan(✉) 
+School of Software Engineering, Tongji University, Shanghai 201804, Chinayuanshijin2003@163.com, tjupengcheng@163.com, binmu@tongji.edu.cn### Abstract.
+
+The abstract should summarize the contents of the paper in short terms, i.e. 150-250 words.
+### Keywords: 
+
+time series prediction, deep neural networks, ENSO.
+### Writing Skill Ref:
+《芝加哥大学论文写作指南》
+《The-Science-of-Scientific-Writing》
+
+### 1	IntroductionTodo1.1 the reason (significance) of study ENSO, traditional methods (statistical method && regular machine learning methods) and limitations.
+
+*"The dynamical models use physical equations of the ocean and atmosphere to forecast ENSO event, which are computationally very expensive and not available outside the at- mospheric scientific community."*
+
+1.2	 deep learning method’s success on data-driven problem
+
+"capturing long-term temporal dependencies" -- LSTM
+
+
+1.3	 ENSO SST prediction is a suitable (reasonable) candidate for deep learning（machine learning） methodRef：
+* 《Prediction of Sea Surface Temperature using Long Short-Term Memory》
+
+
+
+* 《El niño-southern oscillation forecasting using complex networks analysis of LSTM neural networks》 ***（highly related）***
+
+	* Use climate network to construct input and LSTM network as model to predict ENSO, but just priedict single NINO3.4 index, no spatial information discussed, for the experiment, forecast the  NINO3.4 index from 6-month lead, 9-month lead and 12-month lead, use RMSE and MAE, no contrast experiment. the conclusion said that more data and more complex LSTM neural network is needed. 
+
+	* **pyunicorn** --> which is used for construct Climate Network in this paper.
+*  NOAA related blogs
+	
+	* [How Good Have ENSO Forecasts Been Lately?](https://www.climate.gov/news-features/blogs/enso/how-good-have-enso-forecasts-been-lately)( a good reference when inroduce the prediction situation of ENSO )
+
+		* *"how well the forecasts have matched reality"*
+		* lead time --> *" The month from which forecasts are made is often called the start month, and the season that the forecast is for is often called the target season."*
+		* measurements (MAE && Correlation Coefficient)
+		* *"we have a long way to go in improving their performance and utility beyond that.  It is especially hard to predict the timing of ENSO transitions and the correct strength. "** Deep learning
+
+* Prediction of Sea Surface Temperature by Combining Numerical and Neural Techniques
+
+* Using Network Theory and Machine Learning to predict El Niño
+
+* SEASONAL PREDICTION USING UNSUPERVISED FEATURE LEARNING AND REGRESSION
+
+* Statistical Prediction of ENSO from Subsurface Sea Temperature Using a Nonlinear Dimensionality Reduction
+
+* Deep learning for predicting the monsoon over the homogeneous regions of India
+
+* ClimateLearn- A machine-learning approach for climate prediction using network measures
+
+* Bayesian Recurrent Neural Network Models for Forecasting and Quantifying Uncertainty in Spatial-Temporal Data
+
+* 《2018自然基金申请书》
+
+* Unsupervised Discovery of El Nino˜Using Causal Feature Learning on Microlevel Climate Data
+
+
+* Sequence to Sequence Weather Forecasting with Long Short-Term Memory Recurrent Neural Networks
+
+
+### 2	Preliminaries 
+#### 2.1	Formulation of ENSO Prediction Problem
+Todo1.	Goal of ENSO prediction (NINO index and simulation pattern)2.	From the perspective of machine learning， a spatiotemporal regression prediction problemRef：
+* Convolutional LSTM Network- A Machine Learning Approach for Precipitation Nowcasting* #### 2.2	Special Neural Networks for Sequence Modeling
+LSTM and CNN-LSTM network for spatiotemporal prediction, which have show great effectiveness on spatiotemporal problems  1. Structure of CNN and RNN, and combine them together, view as a single variable prediction and a sequence generation problemRef：
+* Deep Learning for Precipitation Nowcasting - A Benchmark and A New Model### 3	Model
+#### 3.1	Multi-layer LSTM Blocks
+Todothe structure of multi LSTM for single variable predictionRef：
+* #### 3.2	CNN –LSTM Spatiotemporal Model
+The structure and detail description of the network (how to capture spatio and temporal structure independently)Ref：
+
+* ### 4 Experiment and Results
+#### 4.1	Single Niño Index Prediction
+Todo1.	Connect those single nino index experiment together
+2.	Compare with different statistical approaches (todo)Ref:
+* Final Report for “Machine learning applications in oceanography” (GitHub)#### 4.2	Grid Spatiotemporal SST Region Prediction
+Todo1.	Different models for grid experiment (todo)Ref:### 5 Conclusion and Future WorkTodo### References
+1.	Author, F.: Article title. Journal 2(5), 99–110 (2016).2.	Author, F., Author, S.: Title of a proceedings paper. In: Editor, F., Editor, S. (eds.) CONFERENCE 2016, LNCS, vol. 9999, pp. 1–13. Springer, Heidelberg (2016). 3.	Author, F., Author, S., Author, T.: Book title. 2nd edn. Publisher, Location (1999).4.	Author, F.: Contribution title. In: 9th International Proceedings on Proceedings, pp. 1–2. Publisher, Location (2010).5.	LNCS Homepage, http://www.springer.com/lncs, last accessed 2016/11/21.
