e3a01623f8319e53898ddc6ea3427bc195b952ff
dl <kris.dacpc@gmail.com>
2018-09-14 10:53:38 +0800

    1.add the log;

--- ENSO/grid_pattern/data_analysis/result_analysis.py
+++ ENSO/grid_pattern/data_analysis/result_analysis.py
@@ -20,29 +20,30 @@ from keras.layers.convolutional import Conv3D
 from keras.layers.convolutional_recurrent import ConvLSTM2D
 from keras.layers.normalization import BatchNormalization
 from keras.models import load_model
+from sklearn.metrics import mean_squared_error
 
 # seq = Sequential()
 # seq.add(ConvLSTM2D(filters=40, kernel_size=(3, 3),
 #                    input_shape=(None, 10, 50, 1),
 #                    padding='same', return_sequences=True))
 # seq.add(BatchNormalization())
-
+#
 # seq.add(ConvLSTM2D(filters=40, kernel_size=(3, 3),
 #                    padding='same', return_sequences=True))
 # seq.add(BatchNormalization())
-
+#
 # seq.add(ConvLSTM2D(filters=40, kernel_size=(3, 3),
 #                    padding='same', return_sequences=True))
 # seq.add(BatchNormalization())
-
+#
 # seq.add(ConvLSTM2D(filters=40, kernel_size=(3, 3),
 #                    padding='same', return_sequences=True))
 # seq.add(BatchNormalization())
-
+#
 # seq.add(Conv3D(filters=1, kernel_size=(3, 3, 3),
 #                activation='sigmoid',
 #                padding='same', data_format='channels_last'))
-
+#
 # seq.compile(loss='mse', optimizer='adadelta')
 # print(seq.summary())
 
@@ -50,21 +51,23 @@ MAX = 31.18499947
 MIN = 20.33499908
 MEAN = 26.80007865
 
-def normalization(data):
-    for i in range(len(data)):
-        for j in range(len(data[0])):
-            data[i][j] = (data[i][j]- MIN)/(MAX - MIN)
-    return data
+def normalization(raw_data):
+    normalize = raw_data
+    for i in range(len(normalize)):
+        for j in range(len(normalize[0])):
+            normalize[i][j] = (normalize[i][j]- MIN)/(MAX - MIN)
+    return normalize
 
-def inverse_normalization(data):
-    for i in range(len(data)):
-        for j in range(len(data[0])):
-            data[i][j] = data[i][j]*(MAX - MIN) + MIN
-    return data
+def inverse_normalization(raw_data):
+    inverse_normalize = raw_data
+    for i in range(len(inverse_normalize)):
+        for j in range(len(inverse_normalize[0])):
+            inverse_normalize[i][j] = inverse_normalize[i][j]*(MAX - MIN) + MIN
+    return inverse_normalize
 
 # data preprocessing
-# sst = '../../../../dataset/sst_grid_1/convert_sst.mon.mean_185001_201512.mat'
-sst = '../../data/sst_grid/convert_sst.mon.mean_1850_01_2015_12.mat'
+sst = '../../../../dataset/sst_grid_1/convert_sst.mon.mean_185001_201512.mat'
+# sst = '../../data/sst_grid/convert_sst.mon.mean_1850_01_2015_12.mat'
 
 sst_data = sio.loadmat(sst)
 sst_data = sst_data['sst'][:,:,:]
@@ -88,39 +91,55 @@ for i in range(167):
 sst_grid = convert_sst
 
 # fit model
-file_path = '../../record/grid_pattern/models/40000withNormalization/40000epoch.h5'
-file_path = '40000epoch.h5'
+# file_path = '../../record/grid_pattern/models/40000withNormalization/40000epoch.h5'
+file_path = '20000epoch.h5'
 
 seq = load_model(file_path)
 
 which_year = 166
-track = sst_grid[which_year][:7, ::, ::, ::]
+
+# precitction
+track1 = sst_grid[which_year][:6, ::, ::, ::]
 
 for j in range(12):
-    new_pos = seq.predict(track[np.newaxis, ::, ::, ::, ::])
+    new_pos = seq.predict(track1[np.newaxis, ::, ::, ::, ::])
     new = new_pos[::, -1, ::, ::, ::]
-    track = np.concatenate((track, new), axis=0)
+    track1 = np.concatenate((track1, new), axis=0)
 
 # to the ground truth
 track2 = sst_grid[which_year][::, ::, ::, ::]
-for i in range(12):
-    fig = plt.figure(figsize=(10, 5))
 
-    ax = fig.add_subplot(121)
-    if i >= 7:
-        ax.text(1, 3, 'Predictions', fontsize=12, color='w')
+for i in range(12):
+    fig = plt.figure(figsize=(20, 6))
+    ax = fig.add_subplot(221)
+    if i >= 6:
+        ax.text(1, 3, 'Prediction', fontsize=12, color='w')
     else:
         ax.text(1, 3, 'Initial trajectory', fontsize=12)
-    toplot = inverse_normalization(track[i, ::, ::, 0])
+    toplot = inverse_normalization(track1[i, ::, ::, 0])
     plt.imshow(toplot)
     cbar = plt.colorbar(plt.imshow(toplot), orientation='horizontal')
     cbar.set_label('°C',fontsize=12)
 
-    ax = fig.add_subplot(122)
+    ax = fig.add_subplot(222)
     plt.text(1, 3, 'Ground truth', fontsize=12)
     toplot = inverse_normalization(track2[i, ::, ::, 0])
     plt.imshow(toplot)
     cbar = plt.colorbar(plt.imshow(toplot), orientation='horizontal')
     cbar.set_label('°C',fontsize=12)
 
-    plt.savefig('%i_animate.png' % (i + 1))
+    ground_truth = track1[i, ::, ::, 0]
+    prediction = track2[i, ::, ::, 0]
+    diff = ground_truth - prediction
+
+    ax = fig.add_subplot(223)
+    ax.text(1, 3, 'Difference', fontsize=12, color='w')
+    toplot = diff
+    plt.imshow(toplot)
+    cbar = plt.colorbar(plt.imshow(toplot), orientation='horizontal')
+    cbar.set_label('°C',fontsize=12)
+
+    mse = mean_squared_error(ground_truth, prediction)
+    print("%i_%i_month total loss: %f" % ( (which_year+1849), (i + 1), mse))
+
+    plt.savefig('%i_%i_month.png' % ((which_year+1849),(i + 1)))
