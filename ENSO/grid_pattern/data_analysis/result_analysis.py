a62c09a9bde087ed47f56e9ba35ab1e2e660ff7b
dl <kris.dacpc@gmail.com>
2018-11-03 10:45:05 +0800

    1.update the structure;

--- ENSO/grid_pattern/data_analysis/result_analysis.py
+++ ENSO/grid_pattern/data_analysis/result_analysis.py
@@ -1,5 +1,5 @@
 '''
-Desc: result analysis of training model.
+Desc: result analysis of the final model.
 Author: Kris Peng
 Data: https://www.esrl.noaa.gov/psd/data/gridded/data.cobe2.html
 Data Desc: 1850/01~2015/12; monthly SST
@@ -9,137 +9,122 @@ Copyright (c) 2018 - Kris Peng <kris.dacpc@gmail.com>
 
 import pylab as plt
 import numpy as np
-import pandas as pd
 import matplotlib as mpl
-import scipy.io as sio
+import os
 import os.path
 from matplotlib import pyplot
-from netCDF4 import Dataset
-from keras.models import Sequential
-from keras.layers.convolutional import Conv3D
-from keras.layers.convolutional_recurrent import ConvLSTM2D
-from keras.layers.normalization import BatchNormalization
 from keras.models import load_model
 from sklearn.metrics import mean_squared_error
-
-# seq = Sequential()
-# seq.add(ConvLSTM2D(filters=40, kernel_size=(3, 3),
-#                    input_shape=(None, 10, 50, 1),
-#                    padding='same', return_sequences=True))
-# seq.add(BatchNormalization())
-#
-# seq.add(ConvLSTM2D(filters=40, kernel_size=(3, 3),
-#                    padding='same', return_sequences=True))
-# seq.add(BatchNormalization())
-#
-# seq.add(ConvLSTM2D(filters=40, kernel_size=(3, 3),
-#                    padding='same', return_sequences=True))
-# seq.add(BatchNormalization())
-#
-# seq.add(ConvLSTM2D(filters=40, kernel_size=(3, 3),
-#                    padding='same', return_sequences=True))
-# seq.add(BatchNormalization())
-#
-# seq.add(Conv3D(filters=1, kernel_size=(3, 3, 3),
-#                activation='sigmoid',
-#                padding='same', data_format='channels_last'))
-#
-# seq.compile(loss='mse', optimizer='adadelta')
-# print(seq.summary())
+from math import sqrt
 
 MAX = 31.18499947
 MIN = 20.33499908
 MEAN = 26.80007865
+train_length = 1800
+len_seq = 1980
+start_seq = 1801
+begin_pred_seq = 12
+end_pred_seq = 24
+DATA_PATH = '../../../../dataset/sst_grid_1/sst_monthly_185001_201512.npy'
+fold_name = 'imgs'
+file_path = "model_2000_epochs.h5"
+map_height, map_width = 10, 50
 
-def normalization(raw_data):
-    normalize = raw_data
-    for i in range(len(normalize)):
-        for j in range(len(normalize[0])):
-            normalize[i][j] = (normalize[i][j]- MIN)/(MAX - MIN)
-    return normalize
+# 0~1 Normalization
+def normalization(data):
+    normalized_data = np.zeros((map_height, map_width), dtype=np.float)
+    for i in range(len(data)):
+        for j in range(len(data[0])):
+            normalized_data[i][j] = (data[i][j]- MIN)/(MAX - MIN)
+    return normalized_data
 
-def inverse_normalization(raw_data):
-    inverse_normalize = raw_data
-    for i in range(len(inverse_normalize)):
-        for j in range(len(inverse_normalize[0])):
-            inverse_normalize[i][j] = inverse_normalize[i][j]*(MAX - MIN) + MIN
-    return inverse_normalize
+def inverse_normalization(data):
+    inverse_data = np.zeros((map_height, map_width), dtype=np.float)
+    for i in range(len(data)):
+        for j in range(len(data[0])):
+            inverse_data[i][j] = data[i][j]*(MAX - MIN) + MIN
+    return inverse_data
 
-# data preprocessing
-sst = '../../../../dataset/sst_grid_1/convert_sst.mon.mean_185001_201512.mat'
-# sst = '../../data/sst_grid/convert_sst.mon.mean_1850_01_2015_12.mat'
+def main():
 
-sst_data = sio.loadmat(sst)
-sst_data = sst_data['sst'][:,:,:]
-sst_data = np.array(sst_data, dtype=float)
-# (180 * 360 * 2004) --> (10 * 50 * 2004) NINO3.4 region (5W~5N, 170W~120W)
-sst_data = sst_data[85:95,190:240,:]
+    train_X_raw, train_Y_raw, sst_grid_raw = np.load(DATA_PATH) # from .npy file
+    # normalization
+    train_X = np.zeros((1980, 24, 10, 50, 1), dtype=np.float)
+    train_Y = np.zeros((1980, 24, 10, 50, 1), dtype=np.float)
+    sst_grid = np.zeros((1981, 24, 10, 50, 1), dtype=np.float)
+    for i in range(1980):
+        for k in range(24):
+            train_X[i,k,::,::,0] = normalization(train_X_raw[i,k,::,::,0])
+            train_Y[i,k,::,::,0] = normalization(train_Y_raw[i,k,::,::,0])
+            sst_grid[i,k,::,::,0] = normalization(sst_grid_raw[i,k,::,::,0])
+    for m in range(24):
+        sst_grid[1980,m,::,::,0] = normalization(sst_grid_raw[1980,m,::,::,0])
 
-# todo
-# # 1850.01~2015.01 (train)
-# train_data = sst_data[::,::,0:-12]
-# # 2015.01~2015.12 (test)
-# test_data = sst_data[::,::,-12:]
+    seq = load_model(file_path)
+    # os.makedirs(fold_name)
 
-# sst min:20.33 / max:31.18
-convert_sst = np.zeros((167,12,10,50,1), dtype = np.float)
-for i in range(167):
-    for k in range(12):
-        # Year * 12 + currentMonth
-        convert_sst[i,k,::,::,0] = normalization(sst_data[::,::,i*12+k])
+    model_sum_loss = 0
+    base_sum_loss = 0
 
-sst_grid = convert_sst
+    for k in range(start_seq, len_seq):
+        # print(k)
+        act_sequence = sst_grid[k][::, ::, ::, ::]
+        pred_sequence = sst_grid[k][:12, ::, ::, ::]
+        print(pred_sequence[np.newaxis, ::, ::, ::, ::].shape)
+        for j in range(12):
+            new_frame = seq.predict(pred_sequence[np.newaxis, ::, ::, ::, ::])
+            # TODO why?? ref: keras conv_lstm.py demo
+            # print(new_frame.shape)
+            new = new_frame[::, -1, ::, ::, ::]
+            pred_sequence = np.concatenate((pred_sequence, new), axis=0)
+        for i in range(begin_pred_seq, end_pred_seq):
+            # fig = plt.figure(figsize=(16, 8))
+            # ax = fig.add_subplot(321)
+            # ax.text(1, 3, 'Prediction', fontsize=12)
+            pred_toplot = inverse_normalization(pred_sequence[i, ::, ::, 0])
+            # plt.imshow(pred_toplot)
+            # cbar = plt.colorbar(plt.imshow(pred_toplot), orientation='horizontal')
+            # cbar.set_label('°C',fontsize=12)
+            # # 将预测seq-12数据作为baseline
+            baseline_frame = inverse_normalization(act_sequence[11, ::, ::, 0])
+            # ax = fig.add_subplot(322)
+            # plt.text(1, 3, 'Baseline', fontsize=12)
+            # plt.imshow(baseline_frame)
+            # cbar = plt.colorbar(plt.imshow(baseline_frame), orientation='horizontal')
+            # cbar.set_label('°C',fontsize=12)
+            # ax = fig.add_subplot(323)
+            # plt.text(1, 3, 'Ground truth', fontsize=12)
+            # act_toplot = inverse_normalization(act_sequence[i, ::, ::, 0])
+            # plt.imshow(act_toplot)
+            # cbar = plt.colorbar(plt.imshow(act_toplot), orientation='horizontal')
+            # cbar.set_label('°C',fontsize=12)
+            # ax = fig.add_subplot(324)
+            # plt.text(1, 3, 'Ground truth', fontsize=12)
+            act_toplot = inverse_normalization(act_sequence[i, ::, ::, 0])
+            # plt.imshow(act_toplot)
+            # cbar = plt.colorbar(plt.imshow(act_toplot), orientation='horizontal')
+            # cbar.set_label('°C',fontsize=12)
+            # ax = fig.add_subplot(325)
+            # plt.text(1, 3, 'Diff_Pred', fontsize=12)
+            # diff_toplot = act_toplot - pred_toplot
+            # plt.imshow(diff_toplot)
+            # cbar = plt.colorbar(plt.imshow(diff_toplot), orientation='horizontal')
+            # cbar.set_label('°C',fontsize=12)
+            # ax = fig.add_subplot(326)
+            # plt.text(1, 3, 'Diff_Base', fontsize=12)
+            # diff_toplot = act_toplot - baseline_frame
+            # plt.imshow(diff_toplot)
+            # cbar = plt.colorbar(plt.imshow(diff_toplot), orientation='horizontal')
+            # cbar.set_label('°C',fontsize=12)
+            # # plt.savefig(fold_name + '/%i_%i_animate.png' % ( (k+1), (i + 1)))
 
-# fit model
-# file_path = '../../record/grid_pattern/models/40000withNormalization/40000epoch.h5'
-file_path = '20000epoch.h5'
+            model_rmse = sqrt(mean_squared_error(act_toplot, pred_toplot))
+            baseline_rmse = sqrt(mean_squared_error(act_toplot, baseline_frame))
+            model_sum_loss, base_sum_loss = model_sum_loss + model_rmse, base_sum_loss + baseline_rmse
 
-seq = load_model(file_path)
+    print("="*10)
+    print("Total Model RMSE: %s" % (model_sum_loss/(12*(len_seq-start_seq))))
+    print("Total Baseline RMSE: %s" % (base_sum_loss/(12*(len_seq-start_seq))))
 
-which_year = 165
-
-# precitction
-track1 = sst_grid[which_year][:6, ::, ::, ::]
-
-for j in range(12):
-    new_pos = seq.predict(track1[np.newaxis, ::, ::, ::, ::])
-    new = new_pos[::, -1, ::, ::, ::]
-    track1 = np.concatenate((track1, new), axis=0)
-
-# to the ground truth
-track2 = sst_grid[which_year][::, ::, ::, ::]
-
-for i in range(12):
-    fig = plt.figure(figsize=(20, 8))
-    ax = fig.add_subplot(221)
-    if i >= 6:
-        ax.text(1, 3, 'Prediction', fontsize=12, color='w')
-    else:
-        ax.text(1, 3, 'Initial trajectory', fontsize=12)
-    toplot = inverse_normalization(track1[i, ::, ::, 0])
-    plt.imshow(toplot)
-    cbar = plt.colorbar(plt.imshow(toplot), orientation='horizontal')
-    cbar.set_label('°C',fontsize=12)
-
-    ax = fig.add_subplot(222)
-    plt.text(1, 3, 'Ground truth', fontsize=12)
-    toplot = inverse_normalization(track2[i, ::, ::, 0])
-    plt.imshow(toplot)
-    cbar = plt.colorbar(plt.imshow(toplot), orientation='horizontal')
-    cbar.set_label('°C',fontsize=12)
-
-    ground_truth = track1[i, ::, ::, 0]
-    prediction = track2[i, ::, ::, 0]
-    diff = ground_truth - prediction
-
-    ax = fig.add_subplot(223)
-    ax.text(1, 3, 'Difference', fontsize=12, color='w')
-    toplot = diff
-    plt.imshow(toplot)
-    cbar = plt.colorbar(plt.imshow(toplot), orientation='horizontal')
-    cbar.set_label('°C',fontsize=12)
-
-    mse = mean_squared_error(ground_truth, prediction)
-    print("%i_%i_month total loss: %f" % ( (which_year+1849), (i + 1), mse))
-
-    plt.savefig('%i_%i_month.png' % ((which_year+1849),(i + 1)))
+if __name__ == '__main__':
+    main()
